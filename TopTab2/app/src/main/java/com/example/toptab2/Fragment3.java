70c353437d59539abd184fb9219ba31df52777cf
youjmen <youjmen17@naver.com>
2020-01-14 18:28:40 +0900

    migration to androidx

---
+++ TopTab2/app/src/main/java/com/example/toptab2/Fragment3.java
@@ -0,0 +1,16 @@
+package com.example.toptab2;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment3, container, false);
+    }
+
+}
